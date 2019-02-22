ff512e9cc954e33ae885511007dfe04c5943f207
Alex Martin <amatin@excilys.com>
2019-02-22 10:43:48 +0100

    Implementing Adding form

--- WebContent/view/addComputer.jsp
+++ WebContent/view/addComputer.jsp
@@ -21,24 +21,26 @@
             <div class="row">
                 <div class="col-xs-8 col-xs-offset-2 box">
                     <h1>Add Computer</h1>
-                    <form action="addComputer" method="POST">
+                    <form action="Add" method="POST">
                         <fieldset>
                             <div class="form-group">
                                 <label for="computerName">Computer name</label>
-                                <input type="text" class="form-control" id="computerName" placeholder="Computer name">
+                                <input type="text" class="form-control" id="computerName" placeholder="Computer name" name="computerName">
                             </div>
                             <div class="form-group">
                                 <label for="introduced">Introduced date</label>
-                                <input type="date" class="form-control" id="introduced" placeholder="Introduced date">
+                                <input type="date" class="form-control" id="introduced" placeholder="Introduced date" name="introduced">
                             </div>
                             <div class="form-group">
                                 <label for="discontinued">Discontinued date</label>
-                                <input type="date" class="form-control" id="discontinued" placeholder="Discontinued date">
+                                <input type="date" class="form-control" id="discontinued" placeholder="Discontinued date" name="discontinued">
                             </div>
                             <div class="form-group">
                                 <label for="companyId">Company</label>
-                                <select class="form-control" id="companyId" >
-                                    <option value="0">--</option>
+                                <select class="form-control" id="companyId" name="companyId">
+                                	<c:forEach var="company" items="${companyList}" varStatus="i">
+                                		<option value="${company.getId()}">${company.getName()}</option>
+                                	</c:forEach>
                                 </select>
                             </div>                  
                         </fieldset>
--- src/main/java/com/excilys/controller/servlets/AddComputer.java
+++ src/main/java/com/excilys/controller/servlets/AddComputer.java
@@ -1,38 +1,93 @@
 package com.excilys.controller.servlets;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.LoggerFactory;
+
+import com.excilys.dao.DaoFactory;
+import com.excilys.dao.model.Company;
+import com.excilys.dao.model.Computer;
+
 /**
  * Servlet implementation class AddComputer
  */
-@WebServlet(name="Add", urlPatterns = {"/Add"})
+@WebServlet(name = "Add", urlPatterns = { "/Add" })
 public class AddComputer extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddComputer() {
-        super();
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public AddComputer() {
+    super();
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    List<Company> companyList = null;
+    try {
+      companyList = DaoFactory.getInstance().getCompanyDao().getAll();
+    } catch (Exception e) {
+      LoggerFactory.getLogger(this.getClass()).warn(e.getMessage());
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	  request.getRequestDispatcher("view/addComputer.jsp").forward(request, response);
-	}
+    request.setAttribute("companyList", companyList);
+    request.getRequestDispatcher("view/addComputer.jsp").forward(request, response);
+  }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // Form validation
+    Computer computer = new Computer();
+    // Name
+    computer.setName(request.getParameter("computerName"));
+    // Introduced
+    try {
+      if(request.getParameter("introduced") != null) {
+        computer.setIntroduced( new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("introduced")));
+      }
+    } catch(Exception e) {
+      LoggerFactory.getLogger(this.getClass()).warn(e.getMessage());
+    }
+    // Discontinued
+    try{
+      if(request.getParameter("discontinued") != null) {
+        computer.setDiscontinued( new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("discontinued")));
+      }
+    } catch(Exception e) {
+      LoggerFactory.getLogger(this.getClass()).warn(e.getMessage());
+    }
+    // Company
+    Company company = null;
+    LoggerFactory.getLogger(this.getClass()).warn(request.getParameter("companyId"));// test
+    long companyId = Long.parseLong(request.getParameter("companyId"));
+    try { 
+      company = DaoFactory.getInstance().getCompanyDao().get(companyId).get();
+    }catch(Exception e) {
+      LoggerFactory.getLogger(this.getClass()).warn(e.getMessage());
+    }
+    computer.setCompany(company);
+
+    
+    // Add
+    LoggerFactory.getLogger(this.getClass()).warn(computer.toString());// test 
+    DaoFactory.getInstance().getComputerDao().save(computer);
+    
+    request.getRequestDispatcher("view/addComputer.jsp").forward(request, response);
+  }
 
 }
