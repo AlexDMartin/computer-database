17165c326ba76aace633671dfc7de5e81669060c
Alex Martin <amartin@excilys.com>
2019-04-15 10:37:57 +0200

    [FEATURE] Add GET computers routes

--- webapp/src/main/java/com/excilys/web/rest/CompanyRestController.java
+++ webapp/src/main/java/com/excilys/web/rest/CompanyRestController.java
@@ -1,8 +1,15 @@
 package com.excilys.web.rest;
 
 import com.excilys.dao.mappers.CompanyMapper;
+import com.excilys.dto.CompanyDto;
 import com.excilys.service.CompanyService;
 import com.excilys.service.ComputerService;
+import com.excilys.service.exception.ServiceException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,28 +33,80 @@ public class CompanyRestController {
     this.companyMapper = companyMapper;
   }
   
-  @GetMapping(value = "/companies", produces = "application/json")
-  public String getAll() {
-    return "{\"test\": \"test\"}";
+  @GetMapping(value = "/companies/{id}", produces = "application/json")
+  public CompanyDto get(@PathVariable int id) {
+    Optional<CompanyDto> companyDto = Optional.empty();
+    
+    try {
+      companyDto = companyService.find(id);
+    } catch (ServiceException serviceException) {
+      // TODO: handle exception
+    }
+    
+    if (companyDto.isPresent()) {      
+      return companyDto.get();
+    }
+    
+    return null;
   }
   
-  @GetMapping(value = "/companies/{id}", produces = "application/json")
-  public String get(@PathVariable int id) {
-    return "{\"test\": \"test\"}";
+  @GetMapping(value = "/companies", produces = "application/json")
+  public List<CompanyDto> getAll() {
+    List<CompanyDto> companies = new ArrayList<>();
+    
+    try {
+      companies = companyService.findAll();
+    } catch (ServiceException serviceException) {
+      // TODO: handle exception
+    }
+    
+    return companies;
   }
     
   @DeleteMapping(value = "/companies/{id}", produces = "application/json")
-  public String delete(@PathVariable int id) {
-    return "{\"test\": \"test\"}";
+  public CompanyDto delete(@PathVariable int id) {
+    Optional<CompanyDto> companyDto = Optional.empty();
+    
+    try {
+      companyDto = companyService.find(id);
+    } catch (ServiceException serviceException) {
+      // TODO: handle exception
+    }
+    
+    if(companyDto.isPresent()) {      
+      try {
+        companyService.delete(companyDto.get());
+      } catch (ServiceException serviceException) {
+        // TODO: handle exception
+      }
+      return companyDto.get();
+    }
+    return null;
   }
   
-  @PostMapping(value = "/companies/{id}", produces = "application/json")
-  public String post(@PathVariable int id) {
-    return "{\"test\": \"test\"}";
+  @PostMapping(value = "/companies", produces = "application/json")
+  public ResponseEntity<CompanyDto> post(@PathVariable CompanyDto companyDto) {
+      if (companyDto != null) {
+        try {
+          companyService.save(companyDto);
+        } catch (ServiceException serviceException) {
+          // TODO: handle exception
+        }
+      }
+      
+      return new ResponseEntity<CompanyDto>(companyDto, HttpStatus.OK);
   }
   
   @PatchMapping(value = "/companies/{id}", produces = "application/json")
-  public String patch(@PathVariable int id) {
-    return "{\"test\": \"test"+ id  +"\"}";
+  public ResponseEntity<CompanyDto> patch(@PathVariable CompanyDto companyDto) {
+    if (companyDto != null) {
+      try {
+        companyService.save(companyDto);
+      } catch (ServiceException serviceException) {
+        // TODO: handle exception
+      }
+    }
+    
+    return new ResponseEntity<CompanyDto>(companyDto, HttpStatus.OK);
   }
 }
--- webapp/src/main/java/com/excilys/web/rest/ComputerRestController.java
+++ webapp/src/main/java/com/excilys/web/rest/ComputerRestController.java
@@ -1,8 +1,13 @@
 package com.excilys.web.rest;
 
 import com.excilys.dao.mappers.ComputerMapper;
+import com.excilys.dto.ComputerDto;
 import com.excilys.service.CompanyService;
 import com.excilys.service.ComputerService;
+import com.excilys.service.exception.ServiceException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,23 +32,59 @@ public class ComputerRestController {
   }
 
   @GetMapping(value = "/computers/{id}", produces = "application/json")
-  public String get(@PathVariable int id) {
-    return "{\"test\": \"test\"}";
+  public ComputerDto get(@PathVariable int id) {
+    Optional<ComputerDto> computerDto = Optional.empty();
+    
+    try {
+      computerDto = computerService.find(id);
+    } catch (ServiceException serviceException) {
+      // TODO: handle exception
+    }
+    
+    if (computerDto.isPresent()) {      
+      return computerDto.get();
+    }
+    
+    return null;
   }
   
   @GetMapping(value = "/computers", produces = "application/json")
-  public String getAll() {
-    return "{\"test\": \"test\"}";
+  public List<ComputerDto> getAll() {
+    List<ComputerDto> computers = new ArrayList<>();
+    
+    try {
+      computers = computerService.findAll();
+    } catch (ServiceException serviceException) {
+      // TODO: handle exception
+    }
+    
+    return computers;
   }
   
   @DeleteMapping(value = "/computers/{id}", produces = "application/json")
-  public String delete(@PathVariable int id) {
-    return "{\"test\": \"test\"}";
+  public ComputerDto delete(@PathVariable int id) {
+    Optional<ComputerDto> computerDto = Optional.empty();
+    
+    try {
+      computerDto = computerService.find(id);
+    } catch (ServiceException serviceException) {
+      // TODO: handle exception
+    }
+    
+    if(computerDto.isPresent()) {      
+      try {
+        computerService.delete(computerDto.get());
+      } catch (ServiceException serviceException) {
+        // TODO: handle exception
+      }
+      return computerDto.get();
+    }
+    return null;
   }
   
   @PostMapping(value = "/computers", produces = "application/json")
   public String post(@PathVariable int id) {
-    return "{\"test\": \"test\"}";
+    return "";
   }
   
   @PatchMapping(value = "/computers/{id}", produces = "application/json")
